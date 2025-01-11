a1b82924d3f1589026ed329acb7338961578854e
LZX0818 <Bahamut-LZX@users.noreply.github.com>
2025-01-11 22:32:29 +0800

    update some custom and build config

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # Modify default password      "password"
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
+# Modify ttyd config
+sed -i 's#/bin/login#/usr/libexec/login.sh#g' feeds/packages/utils/ttyd/files/ttyd.config
+
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
